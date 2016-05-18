0fdb21885a3bd00d93bddb016921b3e292e213fd
sashok58 <sashok58@gmail.com>
2016-05-18 12:23:55 +0300

    Default commit

---
+++ Course/src/com/course/Exercises.java
@@ -0,0 +1,61 @@
+package com.course;
+
+/**
+ * Created by Z on 5/18/2016.
+ */
+public class Exercises {
+    int legsRepeat;
+    double legsWeight;
+    int bodyRepeat;
+    double bodyWeight;
+    int armRepeat;
+    double armWeight;
+
+    public int getLegsRepeat() {
+        return legsRepeat;
+    }
+
+    public void setLegsRepeat(int legsRepeat) {
+        this.legsRepeat = legsRepeat;
+    }
+
+    public double getLegsWeight() {
+        return legsWeight;
+    }
+
+    public void setLegsWeight(double legsWeight) {
+        this.legsWeight = legsWeight;
+    }
+
+    public int getBodyRepeat() {
+        return bodyRepeat;
+    }
+
+    public void setBodyRepeat(int bodyRepeat) {
+        this.bodyRepeat = bodyRepeat;
+    }
+
+    public double getBodyWeight() {
+        return bodyWeight;
+    }
+
+    public void setBodyWeight(double bodyWeight) {
+        this.bodyWeight = bodyWeight;
+    }
+
+    public int getArmRepeat() {
+        return armRepeat;
+    }
+
+    public void setArmRepeat(int armRepeat) {
+        this.armRepeat = armRepeat;
+    }
+
+    public double getArmWeight() {
+        return armWeight;
+    }
+
+    public void setArmWeight(double armWeight) {
+        this.armWeight = armWeight;
+    }
+}
